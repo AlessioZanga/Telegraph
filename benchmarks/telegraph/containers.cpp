ccc82b3b3e0295074569d6be1827d3a9a09f03b4
AlessioZanga <alessio.zanga@outlook.it>
2021-10-25 19:18:44 +0200

    Add FetchContent to CMakeLists

--- benchmarks/telegraph/containers.cpp
+++ benchmarks/telegraph/containers.cpp
@@ -6,6 +6,6 @@ static void BM_SizeConstructor(benchmark::State& state) {
       DenseGraph G(state.range(0));
   }
 }
-BENCHMARK(BM_SizeConstructor)->RangeMultiplier(2)->Range(8, 8<<10);
+BENCHMARK(BM_SizeConstructor)->RangeMultiplier(2)->Range(8, 8<<10)->Complexity(benchmark::oNSquared);
 
 BENCHMARK_MAIN();
