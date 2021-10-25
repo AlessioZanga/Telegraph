cb72a8820fdf540b7c7e81f9631ea8821883784f
AlessioZanga <alessio.zanga@outlook.it>
2021-10-25 18:38:05 +0200

    Add initial support for benchmarks

---
+++ benchmarks/telegraph/containers.cpp
@@ -0,0 +1,11 @@
+#include <benchmark/benchmark.h>
+#include <telegraph/telegraph>
+
+static void BM_SizeConstructor(benchmark::State& state) {
+  for (auto _ : state) {
+      DenseGraph G(state.range(0));
+  }
+}
+BENCHMARK(BM_SizeConstructor)->RangeMultiplier(2)->Range(8, 8<<10);
+
+BENCHMARK_MAIN();
