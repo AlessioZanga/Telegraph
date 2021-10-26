3194b7ce25a2bcd965ef725a298058daca437e55
AlessioZanga <alessio.zanga@outlook.it>
2021-10-27 00:12:33 +0200

    Update benchmarks

--- benchmarks/telegraph/containers.cpp
+++ benchmarks/telegraph/containers.cpp
@@ -1,12 +1,15 @@
 #include <benchmark/benchmark.h>
+
 #include <telegraph/telegraph>
 
-static void SizeConstructor(benchmark::State& state) {
-  for (auto _ : state) {
-      DenseGraph G(state.range(0));
-  }
-  state.SetComplexityN(state.range(0));
+template <typename T>
+void SizeConstructor(benchmark::State& state) {
+    for (auto _ : state) {
+        T G(state.range(0));
+    }
+    state.SetComplexityN(state.range(0));
 }
-BENCHMARK(SizeConstructor)->RangeMultiplier(2)->Range(8, 8<<10)->Complexity(benchmark::oNSquared);
+
+BENCHMARK(SizeConstructor<DirectedDenseGraph>)->RangeMultiplier(2)->Range(8, 8 << 10)->Complexity(benchmark::oNSquared);
 
 BENCHMARK_MAIN();
